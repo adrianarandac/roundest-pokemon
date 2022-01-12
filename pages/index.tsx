ceddbef813d45f169148c374a2b7d24620171fa4
AdrianAranda <adrienaranda@gmail.com>
2022-01-12 09:59:47 +0100

    added default style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='text-2xl text-red-200'>Hello World
+    <div className=''>Hello World
     </div>
   )
 }
