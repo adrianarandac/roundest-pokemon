a38962921e8878000d2f01e0dfde69da63550fe1
AdrianAranda <adrienaranda@gmail.com>
2022-01-18 11:50:44 +0100

    added url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,10 +16,10 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    console.log(process.env.VERCEL_URL);
+
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : `http://${process.env.VERCEL_URL}/api/trpc`;
+      : "http://whostheroundest.vercel.app/api/trpc";
 
     return {
       url,
